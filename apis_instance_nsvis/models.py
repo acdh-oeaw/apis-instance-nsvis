24273e6e7815051b7302c70a0a6898a434c6a030
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-10-24 10:57:50 +0200

    feat(models): add attributes and relations

--- apis_instance_nsvis/models.py
+++ apis_instance_nsvis/models.py
@@ -1,6 +1,9 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 from apis_core.apis_entities.abc import E53_Place, E21_Person
 from apis_core.history.models import VersionMixin
 from apis_core.apis_entities.models import AbstractEntity
+from apis_core.relations.models import Relation
 
 from auditlog.registry import auditlog
 
@@ -10,6 +13,8 @@ class NsvisMixin:
 
 
 class Person(E21_Person, AbstractEntity, VersionMixin):
+    biography = models.TextField(blank=True, verbose_name=_("Biography"))
+
     pass
 
 
@@ -18,9 +23,30 @@ class Place(E53_Place, AbstractEntity, VersionMixin):
 
 
 class Institution(AbstractEntity, VersionMixin):
+    """ Zeitschriften, Verlage, Agenturen, Partei + Vorfeldorganisation """
     pass
 
 
 auditlog.register(Person, serialize_data=True)
 auditlog.register(Place, serialize_data=True)
 auditlog.register(Institution, serialize_data=True)
+
+
+class Collaboration(Relation):
+    subj_model = Person
+    obj_model = Person
+
+
+class Membership(Relation):
+    subj_model = Person
+    obj_model = Institution
+
+
+class LivesIn(Relation):
+    subj_model = Person
+    obj_model = Place
+
+
+class WorksIn(Relation):
+    subj_model = Person
+    obj_model = Place
