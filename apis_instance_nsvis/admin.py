7550bada388fb7da28d8e461bf97ffdfa7cd0d63
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-10-24 14:13:12 +0200

    feat(admin): add MongoDbModel to admin interface

--- apis_instance_nsvis/admin.py
+++ apis_instance_nsvis/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 
-from apis_instance_nsvis.models import Person, Place, Institution
+from apis_instance_nsvis.models import Person, Place, Institution, MongoDbModel
 
 
-@admin.register(Person, Place, Institution)
+@admin.register(Person, Place, Institution, MongoDbModel)
 class NsvisAdmin(admin.ModelAdmin):
     pass
