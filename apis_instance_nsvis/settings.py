60d7904871665df0ccd2f8209d5c364473925a82
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-10-24 09:40:42 +0200

    feat(settings): enable apis_core.relations

--- apis_instance_nsvis/settings.py
+++ apis_instance_nsvis/settings.py
@@ -2,3 +2,4 @@ from apis_acdhch_default_settings.settings import *
 
 INSTALLED_APPS[INSTALLED_APPS.index("apis_ontology")] = "apis_instance_nsvis"
 INSTALLED_APPS += ["auditlog"]
+INSTALLED_APPS.insert(0, "apis_core.relations")
