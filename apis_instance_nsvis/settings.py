b5835b0a6ec59c7af433cd3a561374114911aa0d
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-11-11 12:54:16 +0100

    feat(settings): add `apis_core.history` to `INSTALLED_APPS`

--- apis_instance_nsvis/settings.py
+++ apis_instance_nsvis/settings.py
@@ -2,4 +2,5 @@ from apis_acdhch_default_settings.settings import *
 
 INSTALLED_APPS[INSTALLED_APPS.index("apis_ontology")] = "apis_instance_nsvis"
 INSTALLED_APPS += ["auditlog"]
+INSTALLED_APPS += ["apis_core.history"]
 INSTALLED_APPS.insert(0, "apis_core.relations")
