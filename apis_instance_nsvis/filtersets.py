c807e2276daa09a2eb09e92e0b8255e16a1f0315
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-11-27 14:48:40 +0100

    feat(filterset): implement filters

--- apis_instance_nsvis/filtersets.py
+++ apis_instance_nsvis/filtersets.py
@@ -1,7 +1,35 @@
-from django_filters import UnknownFieldBehavior
+from django.db.models import Q
+from django_filters import UnknownFieldBehavior, FilterSet, MultipleChoiceFilter
 from apis_core.apis_entities.filtersets import AbstractEntityFilterSet
+from apis_instance_nsvis.models import Annotation
 
 
-class AnnotationFilterSet(AbstractEntityFilterSet):
+class CustomMultipleChoiceFilter(MultipleChoiceFilter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.extra["choices"] = self.get_choices()
+
+    def get_choices(self):
+        choices = set()
+        for value in Annotation.objects.exclude(Q(**{f"{self.field_name}__isnull": True})).values_list(self.field_name, flat=True):
+            for val in value:
+                choices.add((val, val))
+        return choices
+
+    def filter(self, qs, value):
+        q = Q()
+        for val in value:
+            q |= Q(**{f"{self.field_name}__contains": [val]})
+        return qs.filter(q)
+
+
+class AnnotationFilterSet(FilterSet):
     class Meta(AbstractEntityFilterSet.Meta):
         unknown_field_behavior = UnknownFieldBehavior.IGNORE
+        fields = {"caption": ["icontains"], "title": ["icontains"], "location": ["icontains"], "other": ["icontains"]}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters["author"] = CustomMultipleChoiceFilter(field_name="author")
+        self.filters["topic"] = CustomMultipleChoiceFilter(field_name="topic")
+        self.filters["depicted"] = CustomMultipleChoiceFilter(field_name="depicted")
