4851b2f991ab26327a8bf2de6bebd7f879b6718d
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-12-03 09:40:50 +0100

    feat(signals): add or create editors group on login

--- apis_instance_nsvis/apps.py
+++ apis_instance_nsvis/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ApisInstanceNsvisConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "apis_instance_nsvis"
+
+    def ready(self):
+        import apis_instance_nsvis.signals
