00a432e14a21e5b9257a36e8d601cea7f7762804
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-12-11 11:30:35 +0100

    feat(filtersets): add additional filters to AnnotationFilterSet

--- apis_instance_nsvis/filtersets.py
+++ apis_instance_nsvis/filtersets.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
-from django_filters import UnknownFieldBehavior, FilterSet, MultipleChoiceFilter
+from django.forms.widgets import CheckboxInput
+from django_filters import UnknownFieldBehavior, FilterSet, MultipleChoiceFilter, BooleanFilter
 from apis_core.apis_entities.filtersets import AbstractEntityFilterSet
 from apis_instance_nsvis.models import Annotation
 
@@ -24,6 +25,35 @@ class CustomMultipleChoiceFilter(MultipleChoiceFilter):
         return qs.filter(q)
 
 
+class IssueFilter(MultipleChoiceFilter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.extra["choices"] = self.get_choices()
+
+    def get_choices(self):
+        issues = sorted(Annotation.objects.values_list(self.field_name, flat=True).distinct())
+        return zip(issues, issues)
+
+    def filter(self, qs, value):
+        q = Q()
+        for val in value:
+            q |= Q(**{f"{self.field_name}__contains": val})
+        return qs.filter(q)
+
+
+class MagazineFilter(IssueFilter):
+    def get_choices(self):
+        issues = sorted(Annotation.objects.values_list(self.field_name, flat=True).distinct())
+        issues = [issue.split(" vom ") for issue in issues]
+        issues = {issue[0] for issue in issues}
+        return zip(issues, issues)
+
+
+class InternalCommentExistsFilter(BooleanFilter):
+    def filter(self, qs, value):
+        return qs.filter(internal_comment__isnull=not value)
+
+
 class AnnotationFilterSet(FilterSet):
     class Meta(AbstractEntityFilterSet.Meta):
         unknown_field_behavior = UnknownFieldBehavior.IGNORE
@@ -31,6 +61,9 @@ class AnnotationFilterSet(FilterSet):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.filters["issue"] = IssueFilter(field_name="issue")
+        self.filters["magazine"] = MagazineFilter(field_name="issue")
         self.filters["author"] = CustomMultipleChoiceFilter(field_name="author")
         self.filters["topic"] = CustomMultipleChoiceFilter(field_name="topic")
         self.filters["depicted"] = CustomMultipleChoiceFilter(field_name="depicted")
+        self.filters["internal_comment"] = InternalCommentExistsFilter(widget=CheckboxInput)
