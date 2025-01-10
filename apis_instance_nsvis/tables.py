fe60fdbdcb8509cd4c4d705a1c6409ea55e0b2c0
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-01-10 11:18:10 +0100

    feat(tables): add table for relations with timespans

--- apis_instance_nsvis/tables.py
+++ apis_instance_nsvis/tables.py
@@ -1,6 +1,25 @@
 import django_tables2 as tables
 from apis_core.apis_entities.tables import AbstractEntityTable
+from apis_core.relations.tables import RelationsListTable
 
 
 class AnnotationTable(AbstractEntityTable):
     labelstudio = tables.TemplateColumn(template_name="columns/labelstudio_link.html")
+
+
+class TimespanTable(RelationsListTable):
+    from_date = tables.Column(order_by="from_date_sort")
+    to_date = tables.Column(order_by="to_date_sort")
+
+    class Meta(RelationsListTable.Meta):
+        sequence = (list(RelationsListTable.Meta.sequence)[:-3] + ["from_date", "to_date"] + list(RelationsListTable.Meta.sequence)[-3:])
+
+
+class PersonAddressDataRelationsTable(TimespanTable):
+    class Meta(TimespanTable.Meta):
+        ...
+
+
+class PersonInstitutionRelationsTable(TimespanTable):
+    class Meta(TimespanTable.Meta):
+        ...
