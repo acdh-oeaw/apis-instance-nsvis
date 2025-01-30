e64b0377b8e7856b2cc6f272571fe153ba680589
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-01-30 08:40:54 +0100

    fix(settings): drop apis_relations removal ...
    ... as this is now already handled in the default settings

--- apis_instance_nsvis/settings.py
+++ apis_instance_nsvis/settings.py
@@ -6,7 +6,6 @@ INSTALLED_APPS += ["apis_core.history"]
 INSTALLED_APPS += ["apis_core.collections"]
 INSTALLED_APPS += ["apis_core.documentation"]
 INSTALLED_APPS.insert(0, "apis_core.relations")
-INSTALLED_APPS.remove("apis_core.apis_relations")
 INSTALLED_APPS += ["apis_acdhch_django_invite"]
 
 ROOT_URLCONF = "apis_instance_nsvis.urls"
