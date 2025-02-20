86e638f6a14fb980a0a1982408052233fe3ff296
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-02-20 13:08:07 +0100

    feat(settings,urls): add & enable apis_acdhch_django_auditlog

--- apis_instance_nsvis/urls.py
+++ apis_instance_nsvis/urls.py
@@ -4,3 +4,4 @@ from django.urls import include, path
 
 urlpatterns += [path("", include("apis_acdhch_django_invite.urls"))]
 urlpatterns += [path("", include("django_interval.urls"))]
+urlpatterns += [path("", include("apis_acdhch_django_auditlog.urls")),]
