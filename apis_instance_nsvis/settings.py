c2348fa992174eb8aaae0807cef4f38e38563214
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-02-20 12:20:15 +0100

    feat(settings, urls): add django_interval to INSTALLED_APPS
    and include its urls in main apis_instance_nsvis.urls

--- apis_instance_nsvis/settings.py
+++ apis_instance_nsvis/settings.py
@@ -9,6 +9,7 @@ INSTALLED_APPS.insert(0, "apis_core.relations")
 INSTALLED_APPS.insert(0, "apis_core.collections")
 INSTALLED_APPS += ["apis_acdhch_django_invite"]
 INSTALLED_APPS += ["django_json_editor_field"]
+INSTALLED_APPS += ["django_interval"]
 
 ROOT_URLCONF = "apis_instance_nsvis.urls"
 
