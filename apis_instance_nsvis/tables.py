5c08ab1c4f83c83a52e2fd0bef843f1af1b36c33
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-03-13 13:13:30 +0100

    feat: improve tables

--- apis_instance_nsvis/tables.py
+++ apis_instance_nsvis/tables.py
@@ -1,3 +1,4 @@
+from datetime import date
 import django_tables2 as tables
 from apis_core.apis_entities.tables import AbstractEntityTable
 from apis_core.relations.tables import RelationsListTable
@@ -15,13 +16,36 @@ class AnnotationTable(AbstractEntityTable):
         fields = ["caption"]
 
 
+class DatesColumn(tables.TemplateColumn):
+    template_name = "apis_instance_nsvis/columns/dates.html"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(template_name=self.template_name, *args, **kwargs)
+
+
 class TimespanTable(RelationsListTable):
-    from_date = tables.TemplateColumn(order_by="from_date_date_sort", template_code="{% if record.from_date %}<abbr title='{{ record.from_date_date_sort }}'>{{ record.from_date }}</abbr>{% endif %}")
-    to_date = tables.Column(order_by="to_date_sort")
+    dates = DatesColumn()
 
     class Meta(RelationsListTable.Meta):
-        sequence = (list(RelationsListTable.Meta.sequence)[:-3] + ["from_date", "to_date"] + list(RelationsListTable.Meta.sequence)[-3:])
-        order_by = ("from_date",)
+        sequence = (list(RelationsListTable.Meta.sequence)[:-3] + ["dates"] + list(RelationsListTable.Meta.sequence)[-3:])
+
+    def sort(self):
+        def sort_date(val):
+            d1 = getattr(val, "date_date_sort", None)
+            d2 = getattr(val, "from_date_date_sort", None)
+            d3 = getattr(val, "to_date_date_sort", None)
+            return d1 or d2 or d3 or date.today()
+        self.rows.data = sorted(self.rows.data, key=sort_date)
+
+    def __init__(self, *args, **kwargs):
+        kwargs["orderable"] = False
+        kwargs["show_header"] = False
+        super().__init__(*args, **kwargs)
+        self.sort()
+
+    def paginate(self, *args, **kwargs):
+        self.sort()
+        super().paginate(*args, **kwargs)
 
 
 class PersonInventoryRelationsTable(TimespanTable):
@@ -38,11 +62,9 @@ class PersonCareerRelationsTable(TimespanTable):
     details = tables.Column()
 
     class Meta(TimespanTable.Meta):
-        sequence = (list(TimespanTable.Meta.sequence)[:-5] + ["details"] + list(TimespanTable.Meta.sequence)[-5:])
+        sequence = (list(TimespanTable.Meta.sequence)[:-4] + ["details"] + list(TimespanTable.Meta.sequence)[-4:])
 
 
 class PersonPlacesRelationsTable(TimespanTable):
-    date = tables.Column(order_by="date")
-
     class Meta(TimespanTable.Meta):
-        sequence = (list(TimespanTable.Meta.sequence)[:-5] + ["date"] + list(TimespanTable.Meta.sequence)[-5:])
+        ...
