8c08769e9dab096f972a082dd461c74c2b941a7f
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-04-22 10:50:18 +0200

    feat(views): add view listing annotations where nr of authors if off

--- apis_instance_nsvis/urls.py
+++ apis_instance_nsvis/urls.py
@@ -2,6 +2,10 @@ from apis_acdhch_default_settings.urls import urlpatterns
 
 from django.urls import include, path
 
+from apis_instance_nsvis.views import WrongAnnotationNumber
+
 urlpatterns += [path("", include("apis_acdhch_django_invite.urls"))]
 urlpatterns += [path("", include("django_interval.urls"))]
 urlpatterns += [path("", include("apis_acdhch_django_auditlog.urls")),]
+
+urlpatterns += [path("apis/apis_instance_nsvis.annotation/wrongnumberofauthors", WrongAnnotationNumber.as_view(), name="wrongnumberofauthors")]
