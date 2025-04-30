2b41715a5386ed4aee172e8c25f1b1baec128697
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-04-30 10:13:16 +0200

    fix(settings): drop apps that are shipped with default settings

--- apis_instance_nsvis/settings.py
+++ apis_instance_nsvis/settings.py
@@ -3,9 +3,7 @@ from apis_acdhch_default_settings.settings import *
 
 INSTALLED_APPS[INSTALLED_APPS.index("apis_ontology")] = "apis_instance_nsvis"
 INSTALLED_APPS += ["auditlog"]
-INSTALLED_APPS += ["apis_core.history"]
 INSTALLED_APPS += ["apis_core.documentation"]
-INSTALLED_APPS.insert(0, "apis_core.collections")
 INSTALLED_APPS += ["apis_acdhch_django_invite"]
 INSTALLED_APPS += ["django_json_editor_field"]
 INSTALLED_APPS += ["django_interval"]
