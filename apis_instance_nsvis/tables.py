4b23b81de153bc9d61c6762797b1d5328a8b7ba2
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-05-26 09:41:21 +0200

    feat(models): add ranking to annotation and show ranking in table

--- apis_instance_nsvis/tables.py
+++ apis_instance_nsvis/tables.py
@@ -13,6 +13,7 @@ class AnnotationTable(AbstractEntityTable):
     clip = tables.TemplateColumn(template_code="<img class='thumbnail' src={{ record.clip }}>", exclude_from_export=True)
     author = tables.TemplateColumn(template_name="columns/annotation_author.html")
     fotographers = tables.TemplateColumn(template_name="columns/annotation_fotographers.html")
+    ranking = tables.TemplateColumn(template_code="{{ record.ranking|floatformat:-3 }}")
 
     class Meta(AbstractEntityTable.Meta):
         exclude = ["desc"]
