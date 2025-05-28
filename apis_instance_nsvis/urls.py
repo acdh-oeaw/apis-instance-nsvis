807ca728b8a2370c292602f2781cb7b5fbb87f52
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-05-28 11:48:07 +0200

    refactor(urls): import view module instead of individual views

--- apis_instance_nsvis/urls.py
+++ apis_instance_nsvis/urls.py
@@ -2,12 +2,12 @@ from apis_acdhch_default_settings.urls import urlpatterns
 
 from django.urls import include, path
 
-from apis_instance_nsvis.views import WrongAnnotationNumber, AnnotationAuthorsView, AnnotationReportsView
+from apis_instance_nsvis import views
 
 urlpatterns += [path("", include("apis_acdhch_django_invite.urls"))]
 urlpatterns += [path("", include("django_interval.urls"))]
 urlpatterns += [path("", include("apis_acdhch_django_auditlog.urls")),]
 
-urlpatterns += [path("apis/apis_instance_nsvis.annotation/wrongnumberofauthors", WrongAnnotationNumber.as_view(), name="wrongnumberofauthors")]
-urlpatterns += [path("apis/<contenttype:contenttype>/authors", AnnotationAuthorsView.as_view(), name="annotationsauthorsview")]
-urlpatterns += [path("apis/<contenttype:contenttype>/reports", AnnotationReportsView.as_view(), name="annotationsreportsview")]
+urlpatterns += [path("apis/apis_instance_nsvis.annotation/wrongnumberofauthors", views.WrongAnnotationNumber.as_view(), name="wrongnumberofauthors")]
+urlpatterns += [path("apis/<contenttype:contenttype>/authors", views.AnnotationAuthorsView.as_view(), name="annotationsauthorsview")]
+urlpatterns += [path("apis/<contenttype:contenttype>/reports", views.AnnotationReportsView.as_view(), name="annotationsreportsview")]
