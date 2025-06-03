22aed0914bc690dc16ac31ae60e4bc718157a7b2
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-06-03 08:22:37 +0200

    feat(models): rename `fotographers` to `photographers`

--- apis_instance_nsvis/models.py
+++ apis_instance_nsvis/models.py
@@ -187,7 +187,7 @@ class Annotation(AbstractEntity):
     topic = ArrayField(models.CharField(), null=True)
     other = models.TextField(blank=True, null=True)
     internal_comment = models.TextField(blank=True, null=True)
-    fotographers = models.JSONField(null=True, editable=False)
+    photographers = models.JSONField(null=True, editable=False)
 
     class Meta:
         ordering = ["pk"]
