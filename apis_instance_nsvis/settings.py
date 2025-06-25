33014bcfd13789e2c082ef9e7a1f87f0c2b774dd
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-06-25 11:46:39 +0200

    feat(settings): add apis_instance_nsvis to the lookup path
    We have to put our own module *before* the default settings in the
    module lookup path, so we can override the importer module.

--- apis_instance_nsvis/settings.py
+++ apis_instance_nsvis/settings.py
@@ -19,3 +19,5 @@ MIDDLEWARE += [  # noqa: F405
     "auditlog.middleware.AuditlogMiddleware",
     "simple_history.middleware.HistoryRequestMiddleware",
 ]
+
+ADDITIONAL_MODULE_LOOKUP_PATHS = ["apis_instance_nsvis", "apis_acdhch_default_settings"]
