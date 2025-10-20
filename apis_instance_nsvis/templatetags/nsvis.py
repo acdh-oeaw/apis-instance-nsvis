d410b8130003875d41639ed2ac45bddab741266b
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-10-20 13:10:53 +0200

    feat(templatetags): introduce templatetags for imgproxy access

--- apis_instance_nsvis/templatetags/nsvis.py
+++ apis_instance_nsvis/templatetags/nsvis.py
@@ -1,5 +1,6 @@
 import json
 from django import template
+from apis_instance_nsvis.utils import MyImgProxy, Magazines
 
 register = template.Library()
 
@@ -7,3 +8,23 @@ register = template.Library()
 @register.filter
 def pretty_json(value):
     return json.dumps(value, indent=2)
+
+
+@register.filter
+def get_imgproxy_url_for_labelled_url(url):
+    magazines = Magazines()
+    path = magazines.get_path_for_url(url)
+    imgproxy = MyImgProxy()
+    if path:
+        return imgproxy.calc(f"23503/new/{path}.jpg")
+    return None
+
+
+@register.filter
+def get_imgproxy_thumbnail_for_labelled_url(url):
+    magazines = Magazines()
+    path = magazines.get_path_for_url(url)
+    imgproxy = MyImgProxy()
+    if path:
+        return imgproxy.resize(f"23503/new/{path}.jpg")
+    return None
