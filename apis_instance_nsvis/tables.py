42c4f88a2fffc4eec7247f3a17e7b5ec85bd34ac
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2026-04-20 09:43:08 +0200

    fix: reorder columns in relation tables
    remove action column in view tables

--- apis_instance_nsvis/tables.py
+++ apis_instance_nsvis/tables.py
@@ -1,19 +1,31 @@
 from datetime import date
+
 import django_tables2 as tables
+
 from apis_core.generic.tables import GenericTable
 from apis_core.relations.tables import RelationsListTable
-from apis_instance_nsvis.models import Annotation
 
 
 class AnnotationTable(GenericTable):
-    issue = tables.TemplateColumn(template_code="<a href={{ record.get_absolute_url }}>{{ record.magazine_page.issue }}</a>")
-    labelstudio = tables.TemplateColumn(template_name="columns/labelstudio_link.html", exclude_from_export=True)
+    issue = tables.TemplateColumn(
+        template_code="<a href={{ record.get_absolute_url }}>{{ record.magazine_page.issue }}</a>"
+    )
+    labelstudio = tables.TemplateColumn(
+        template_name="columns/labelstudio_link.html", exclude_from_export=True
+    )
     topic = tables.TemplateColumn(template_code="{{ record.topic|join:', ' }}")
     depicted = tables.TemplateColumn(template_code="{{ record.depicted|join:', ' }}")
-    clip = tables.TemplateColumn(template_code="<img class='thumbnail' src='{{ record.clip }}' loading='lazy' onerror='imgError(this);'>", exclude_from_export=True)
+    clip = tables.TemplateColumn(
+        template_code="<img class='thumbnail' src='{{ record.clip }}' loading='lazy' onerror='imgError(this);'>",
+        exclude_from_export=True,
+    )
     author = tables.TemplateColumn(template_name="columns/annotation_author.html")
-    photographers = tables.TemplateColumn(template_name="columns/annotation_photographers.html")
-    ranking = tables.TemplateColumn(template_code='<abbr title="{{ record.ranking }}">{{ record.ranking|floatformat:-3 }}</abbr>')
+    photographers = tables.TemplateColumn(
+        template_name="columns/annotation_photographers.html"
+    )
+    ranking = tables.TemplateColumn(
+        template_code='<abbr title="{{ record.ranking }}">{{ record.ranking|floatformat:-3 }}</abbr>'
+    )
     caption = tables.Column(attrs={"td": {"class": "col-1"}})
 
     class Meta(GenericTable.Meta):
@@ -32,7 +44,11 @@ class TimespanTable(RelationsListTable):
     dates = DatesColumn()
 
     class Meta(RelationsListTable.Meta):
-        sequence = (list(RelationsListTable.Meta.sequence)[:-3] + ["dates"] + list(RelationsListTable.Meta.sequence)[-3:])
+        sequence = (
+            list(RelationsListTable.Meta.sequence)[:-3]
+            + ["dates"]
+            + list(RelationsListTable.Meta.sequence)[-3:]
+        )
 
     def sort(self):
         def sort_date(val):
@@ -40,11 +56,16 @@ class TimespanTable(RelationsListTable):
             d2 = getattr(val, "from_date_date_sort", None)
             d3 = getattr(val, "to_date_date_sort", None)
             return d1 or d2 or d3 or date.today()
+
         self.rows.data = sorted(self.rows.data, key=sort_date)
 
     def __init__(self, *args, **kwargs):
         kwargs["orderable"] = False
         kwargs["show_header"] = False
+        if "/update/" not in kwargs["request"].path:
+            self._meta.exclude = RelationsListTable.Meta.exclude + ("actions",)
+        else:
+            self._meta.exclude = RelationsListTable.Meta.exclude
         super().__init__(*args, **kwargs)
         self.sort()
 
@@ -57,31 +78,31 @@ class AbstractEntityInventoryRelationsTable(TimespanTable):
     extent = tables.Column()
     contact = tables.Column()
 
-    class Meta(TimespanTable.Meta):
-        ...
+    class Meta(TimespanTable.Meta): ...
 
 
 class AbstractEntityOrganizationRelationsTable(TimespanTable):
     details = tables.Column()
 
-    class Meta(TimespanTable.Meta):
-        ...
+    class Meta(TimespanTable.Meta): ...
 
 
 class AbstractEntityCareerRelationsTable(TimespanTable):
     details = tables.Column()
 
-    class Meta(TimespanTable.Meta):
-        sequence = (list(TimespanTable.Meta.sequence)[:-4] + ["details"] + list(TimespanTable.Meta.sequence)[-4:])
+    #  class Meta(TimespanTable.Meta):
+    #      sequence = (list(TimespanTable.Meta.sequence)[:-4] + ["details"] + list(TimespanTable.Meta.sequence)[-4:])
+    class Meta(TimespanTable.Meta): ...
 
 
 class AbstractEntityPlacesRelationsTable(TimespanTable):
-    class Meta(TimespanTable.Meta):
-        ...
+    class Meta(TimespanTable.Meta): ...
 
 
 class MagazineIssueTable(GenericTable):
-    pages = tables.TemplateColumn(template_code="{{ record.magazinepage_set.count }}", orderable=False)
+    pages = tables.TemplateColumn(
+        template_code="{{ record.magazinepage_set.count }}", orderable=False
+    )
     annotations = tables.Column(empty_values=(), orderable=False)
     topics = tables.Column(empty_values=(), orderable=False)
     depicted = tables.Column(empty_values=(), orderable=False)
